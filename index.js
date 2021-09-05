6d13f31c83113360053bb05dbb7b940ef2250c34
Roman Korotaev <kro1986@ukr.net>
2021-09-05 20:17:22 +0300

    01

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
 console.log("Test")
 
-// try {
-//     console.log("Внутри try  до myVar;")
-//     myVar;
-
-//      console.log("Внутри try  после myVar;")
-
-// } catch (error) {
-//     // Пример ошибки. Рассматриваем этот объект
-//     // console.dir(error.name)
-//     // console.log(error.message)
-//      console.log(error.stack)
-//      console.log("Ошибка!")
-// }
-
-// console.log("После try...catch")
- 
-
-
- //----------------------
-
-// const validJSON = '{"name": "Mango", "age": 3 }';
-// const invalidJSON = '{бекенд вернул вот такое чудо}';
-
-// try {
-//     console.log(JSON.parse(invalidJSON));
-// } catch (error) {
-//     console.log(error);
-//     console.log("Ошибка!")
-//     if (error.name === "SyntaxError") {
-//         console.log("Ошибка парса JSON , надо что-то делать!")
-//     }
-// }
-
-//  console.log("После try...catch")
-
-
  //---------------------- Асинхронные функции   async  -----------------
 
 
